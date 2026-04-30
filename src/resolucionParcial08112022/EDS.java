ab16024236fad7a3798c21778f83229bc4400176
Leonardo Dávalos <davaloslm@hotmail.com.ar>
2026-04-30 20:15:02 -0300

    Add: item b ejercicio de objetos parcial 08 11 2022

--- src/resolucionParcial08112022/EDS.java
+++ src/resolucionParcial08112022/EDS.java
@@ -3,4 +3,29 @@ package resolucionParcial08112022;
 
 public class EDS {
 	Proyecto[] proyectos;
+	
+	//b)
+	public Software elaboradoPorExpertos() {
+		Software software = null;
+		int cantMaxExpertos = 0;
+		
+		for (Proyecto p : proyectos) {
+			int contadorExpertos = 0;
+			
+			//contamos expertos
+			for (Desarrollador d : p.desarrolladores) {
+				if (d.experiencia >= 10) {
+					contadorExpertos++;
+				}
+			}
+			
+			//hallamos el sw con mas expertos
+			if (contadorExpertos >= cantMaxExpertos) {
+				cantMaxExpertos = contadorExpertos;
+				software = p.producto;
+			}
+		}
+		
+		return software;
+	}
 }
--- src/resolucionParcial13062023/Funcion.java
+++ src/resolucionParcial13062023/Funcion.java
@@ -8,17 +8,5 @@ public class Funcion {
 	Date horaInicio; //La consigna dice Time horaInicio, pero Java no tiene ese tipo de dato. Cambio a Date solo para que compile.
 	Sector[] sectores;
 	
-	
 
-	//metodo auxiliar para el punto b
-	public boolean estaAgotada() {
-		for (Sector sector : sectores) {
-			for (Asiento asiento : sector.asientos) {
-				if (asiento.usuario == null) { //si encuentro un asiento libre, ya sabemos que no esta agotada. retorno false
-					return false;
-				}
-			}
-		}
-		return true; //si recorro todos los asientos y no encuentro ninguno libre, esta agotada. retorno true
-	}
 }
